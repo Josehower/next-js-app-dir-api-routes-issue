b02c5e7fc356b342e53e87d0d94c750f674f0ccc
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-02-15 17:45:19 +0100

    simplify error

--- app/page.js
+++ app/page.js
@@ -1,44 +1,24 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  const [error, setError] = useState();
+
   return (
-    <>
-      {error ? (
-        <div>
-          {error} <br />
-          <button
-            onClick={async () => {
-              router.refresh();
-            }}
-          >
-            refresh
-          </button>
-        </div>
-      ) : (
-        <button
-          onClick={async () => {
-            try {
-              const response = await fetch('/api', { method: 'POST' });
-              const data = response.json();
+    <button
+      onClick={async () => {
+        try {
+          const response = await fetch('/api', { method: 'POST' });
+          const data = response.json();
 
-              router.push(data.url);
-            } catch (error) {
-              console.error(error);
-              setError(`
-              ${error.message}.
-
-              refresh to see the app is broken
-              `);
-            }
-          }}
-        >
-          Fetch /api
-        </button>
-      )}
-    </>
+          router.push(data.url);
+        } catch (error) {
+          console.error(error);
+          router.refresh();
+        }
+      }}
+    >
+      Fetch /api
+    </button>
   );
 }
